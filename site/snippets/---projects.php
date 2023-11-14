5a51da79b2e448482f2d4035e646b342d8334c32
Giulia Polenta <giuliapolenta@gmail.com>
2023-11-14 14:50:47 +0100

    Delete projects and tools snippets

--- site/snippets/---projects.php
+++
@@ -1,9 +0,0 @@
-<main id="projects" class="grid-layout main">
-    <ul class="scroll-wrapper">
-        <?php foreach ($pages->template('projects')->children() as $page) : ?> 
-            <li class="grid-layout-title" <?php if ($cover = $page->background()->toFile()): ?>style="background-image: url('<?= $cover->url() ?>');"<?php endif ?>>
-                <h2><a href="<?= $page->url() ?>"><?= $page->title()->kt() ?></a></h2>
-            </li>
-        <?php endforeach ?>
-    </ul>
-</main>
