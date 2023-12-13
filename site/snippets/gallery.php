32337f5d25c992708fd9d6dcfb64516a9f7ede67
Giulia Polenta <giuliapolenta@gmail.com>
2023-12-13 16:42:36 +0100

    Upload project media; fix style in gallery accordingly

--- site/snippets/gallery.php
+++ site/snippets/gallery.php
@@ -1,20 +1,95 @@
 <?php if ($media->type() == 'image') : ?>
     <figure class="gallery-item image-item" data-type="<?= $media->mediatype()->slug() ?>" data-category="<?= $media->filter()->slug() ?>">
         <img src="<?= $media->resize(1200, null)->url() ?>" alt="<?= $media->alt() ?>">
-        <?= snippet('caption', ['media' => $media]) ?>
+        <figcaption>
+            <div class="media-text-wrapper">
+                <div class="media-summary text-label">
+                    <?php if ($media->mediatype()->isNotEmpty()): ?>
+                        <p>Media type: <?= $media->mediatype() ?></p>
+                    <?php endif ?>
+                    <?php if ($media->filter()->isNotEmpty()): ?>
+                        <p>Process stage: <?= $media->filter() ?></p>
+                    <?php endif ?>
+                    <?php if ($slots->filterByProject()): ?>
+                        <p>Project: <a href="<?= $media->parent()->url()?>"><?= $media->parent()->title()?></a></p>
+                    <?php endif ?>
+                </div>
+                <?php if ($media->caption()->isNotEmpty()) : ?>
+                    <hr>
+                    <div class="media-credits text-label">
+                        <?= $media->caption()->kt() ?>
+                    </div>
+                    <?php endif ?>
+                <?php if ($media->info()->isNotEmpty()): ?>
+                    <div class="media-info text">
+                        <?= $media->info()->kt() ?>
+                    </div>
+                <?php endif ?>
+            </div>
+        </figcaption>
     </figure>
 <?php endif ?> 
 
 <?php if ($media->type() == 'video') : ?>
     <figure class="gallery-item video-item" data-type="<?= $media->mediatype()->slug() ?>" data-category="<?= $media->filter()->slug() ?>">
         <video src="<?= $media->url() ?>" autoplay muted loop controlslist="noplaybackrate nodownload" disablePictureInPicture type="video"></video>
-        <?= snippet('caption', ['media' => $media]) ?>
+        <figcaption>
+            <div class="media-text-wrapper">
+                <div class="media-summary text-label">
+                    <?php if ($media->mediatype()->isNotEmpty()): ?>
+                        <p>Media type: <?= $media->mediatype() ?></p>
+                    <?php endif ?>
+                    <?php if ($media->filter()->isNotEmpty()): ?>
+                        <p>Process stage: <?= $media->filter() ?></p>
+                    <?php endif ?>
+                    <?php if ($slots->filterByProject()): ?>
+                        <p>Project: <a href="<?= $media->parent()->url()?>"><?= $media->parent()->title()?></a></p>
+                    <?php endif ?>
+                </div>
+                <?php if ($media->caption()->isNotEmpty()) : ?>
+                    <hr>
+                    <div class="media-credits text-label">
+                        <?= $media->caption()->kt() ?>
+                    </div>
+                    <?php endif ?>
+                <?php if ($media->info()->isNotEmpty()): ?>
+                    <div class="media-info text">
+                        <?= $media->info()->kt() ?>
+                    </div>
+                <?php endif ?>
+            </div>
+        </figcaption>
     </figure>
 <?php endif ?> 
 
 <?php if ($media->type() == 'audio') : ?>
     <figure class="gallery-item audio-item" data-type="<?= $media->mediatype()->slug() ?>" data-category="<?= $media->filter()->slug() ?>">
         <audio src="<?= $media->url() ?>" controls controlslist="noplaybackrate nodownload" preload="metadata" type="audio"></audio>
-        <?= snippet('caption', ['media' => $media]) ?>
+        <figcaption>
+            <div class="media-text-wrapper">
+                <div class="media-summary text-label">
+                    <?php if ($media->mediatype()->isNotEmpty()): ?>
+                        <p>Media type: <?= $media->mediatype() ?></p>
+                    <?php endif ?>
+                    <?php if ($media->filter()->isNotEmpty()): ?>
+                        <p>Process stage: <?= $media->filter() ?></p>
+                    <?php endif ?>
+                    <?php if ($slots->filterByProject()): ?>
+                        <p>Project: <a href="<?= $media->parent()->url()?>"><?= $media->parent()->title()?></a></p>
+                    <?php endif ?>
+                </div>
+                <?php if ($media->caption()->isNotEmpty()) : ?>
+                    <hr>
+                    <div class="media-credits text-label">
+                        <?= $media->caption()->kt() ?>
+                    </div>
+                    <?php endif ?>
+                <?php if ($media->info()->isNotEmpty()): ?>
+                    <div class="media-info text">
+                        <?= $media->info()->kt() ?>
+                    </div>
+                <?php endif ?>
+            </div>
+        </figcaption>
     </figure> 
 <?php endif ?>
